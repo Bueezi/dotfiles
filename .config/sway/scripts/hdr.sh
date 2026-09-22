7c9b90a2c4b3268eb2d9c80e629c6838fff46c8b
bueezi <mail@b3n.me>
2026-09-22 13:54:39 +0200

    update

---
+++ .config/sway/scripts/hdr.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+state="${XDG_RUNTIME_DIR:-/tmp}/sway-hdr"
+
+if [ -f "$state" ]; then
+    swaymsg output '*' hdr off
+    rm -f "$state"
+    notify-send "HDR off"
+else
+    swaymsg output '*' hdr on
+    touch "$state"
+    notify-send "HDR on"
+fi
--- .config/sway/scripts/theme.sh
+++ .config/sway/scripts/theme.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 # GTK Theme Switcher using fuzzel
 
 THEMES=(
